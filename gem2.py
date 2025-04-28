b67c71d4c52af11bff1cc784382d945ccf156fba
Ricardo Filgueiras <107813151+Ricardo-Filgueiras@users.noreply.github.com>
2025-04-28 15:49:14 -0300

    refinando o script

--- gem2.py
+++ gem2.py
@@ -12,7 +12,6 @@ from langchain_google_genai import ChatGoogleGenerativeAI, GoogleGenerativeAIEmb
 
 load_dotenv()
 
-# 1. carregando os dados
 loader = CSVLoader(file_path="dadosbasev1.csv" , encoding="utf-8")
 documents = loader.load()
 print(f"Total de documentos: {len(documents)}")
@@ -22,7 +21,7 @@ embeddings = GoogleGenerativeAIEmbeddings( google_api_key=os.getenv("GOOGLE_API_
 vectorstore = FAISS.from_documents(documents, embeddings)
 retireval = vectorstore.as_retriever()
 
-print(f"embedding ok")
+
 # 2.1 Inicializando o modelo de linguagem Gemini
 llm = ChatGoogleGenerativeAI(
     model="gemini-2.0-flash",
@@ -30,7 +29,7 @@ llm = ChatGoogleGenerativeAI(
     google_api_key=os.getenv("GOOGLE_API_KEY"),
 )
 
-print(f"gemini ok")
+
 
 rag_templalte = """
 voce é um assistente pessoal de um cliente de uma empresa de tecnologia.
@@ -43,7 +42,7 @@ Pergunta:
 {question}
 """
 
-print(f"prompt ok")
+
 
 prompt = ChatPromptTemplate.from_template(rag_templalte)
 chain = (
@@ -51,23 +50,8 @@ chain = (
     | prompt
     | llm
 )
-print(f"chain ok")
 
-# chain.invoke({"question": "Qual é o seu hobby?"})
-# Sua pergunta
 
-'''
-
-minha_pergunta = "Qual seu animal favorito ?"
-
-# Invocando a chain com a pergunta
-resposta = chain.invoke(minha_pergunta)
-
-# Exibindo a resposta
-print(f"\nPergunta: {minha_pergunta}")
-print(f"Resposta: {resposta.content}")
-
-'''
 # Vamos conversar com o modelo
 def conversar_com_modelo(pergunta):
     print("Bem-vindo ao seu assistente vamos Conversando com o modelo... digite 'sair' para encerrar.\n")
